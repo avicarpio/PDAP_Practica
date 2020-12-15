4a275c93edb2dd4f75bf5f8f0c65ce8d60f8a71d
Alex <alex.vicente@students.salle.url.edu>
2020-12-15 11:57:45 +0100

    Classficadors, extraccio i db
    AudioFeatureExtractor configurat.
    ADS Configurat segons paràmetre d'entrada.
    Tall de cada ADS i extracció de les features per cada un.
    Extracció de les labels de cada ADS a la matriu Labels.
    
    Classficiadors KNN, CART i SVM configurats per entrenament.

--- practica_audio.m
+++ practica_audio.m
@@ -1,6 +1,7 @@
 path = "audioDB";%Adreça a la carpeta que conté la base de dades d’àudio
-methodML = "" ;% knn , cart , svm , gmm
-PercentOfDataSet = 0; 
+fs = 44.1e3;
+methodML = "knn" ;% knn , cart , svm , gmm
+PercentOfDataSet = 5; 
 % Segons el classificador que escullen s'han d'inizialitzar els parametres
 % corresponents
 switch methodML
@@ -12,7 +13,7 @@ switch methodML
     case 'cart'
         cart_showModel = 0;
     case 'svm'
-            
+        t = templateSVM('KernelFunction','rbf','Standardize',true);    
     case 'gmm'
 
 end
@@ -26,22 +27,88 @@ ads = audioDatastore(path, ...
     'FileExtensions','.wav', ...
     'LabelSource','foldernames');
 
-adsTall = tall(ads);
+aFE = audioFeatureExtractor("SampleRate",fs,...
+    "Window",hamming(round(0.035*fs),"periodic"),...
+    "OverlapLength",round(0.0175*fs),...
+    "barkSpectrum",true,"mfcc",true,...
+    "spectralCentroid",true,"spectralCrest",true,...
+    "spectralKurtosis",true,"spectralSpread",true);
 
-%Parametrització de la bbdd mitjançant atributs d'audio (3.2) 
-fileName = "";
-stWin = 0.2;        % O.2
-stStep = 0.1;       % 0.1
-mtWin= 2;           % 2
-mtStep = 1;         % 1
-featureShort = {'ZCR', 'energy', 'enEntropy', 'specCentroid', 'specSpread', 'specEntropy', 'specFlux'}; % Heu de triar els q vosaltres creieu
-featureStatistics = {'stdbymean', 'median', 'std', 'meanNonZero', 'max', 'min', 'medianNonZero'}; % Heu de triar els q vosaltres creieu
-audioFeatureExtraction(fileName, stWin, stStep, mtWin, mtStep, featureStatistics, featureShort);
+ads_reduit = splitEachLabel(ads,PercentOfDataSet/100);
+
+%Extracció dades (features i labels)
+
+[ads1,ads2,ads3,ads4] = splitEachLabel(ads_reduit,0.25,0.25,0.25);
+
+ads1Tall = tall(ads1);
+ads2Tall = tall(ads2);
+ads3Tall = tall(ads3);
+ads4Tall = tall(ads4);
+
+specsTall = cellfun(@(x)extract(aFE,x),ads1Tall,"UniformOutput",false);
+features1_Tall = gather(specsTall);
+
+specsTall = cellfun(@(x)extract(aFE,x),ads2Tall,"UniformOutput",false);
+features2_Tall = gather(specsTall);
+
+specsTall = cellfun(@(x)extract(aFE,x),ads3Tall,"UniformOutput",false);
+features3_Tall = gather(specsTall);
+
+specsTall = cellfun(@(x)extract(aFE,x),ads4Tall,"UniformOutput",false);
+features4_Tall = gather(specsTall);
+
+features = cell(1,4);
+labels = cell(1,4);
+
+features{1} = gather(features1_Tall);
+labels{1} = ads1.Labels;
+
+features{2} = gather(features2_Tall);
+labels{2} = ads2.Labels;
+
+features{3} = gather(features3_Tall);
+labels{3} = ads3.Labels;
+
+features{4} = gather(features4_Tall);
+labels{4} = ads4.Labels;
+
+%1st Cell
+%TO-DO Tenim una matriu features que conté un array d'arrays de N elements
+%i M mostres (finestra) per cada N. Fer que tot estigui en una sola
+%columna, i repetir cada label per cada mostra, per a que cada una estigui
+%etiquetada correctament.
+
+dbFull = reshape(features{1}{1},[],1);
+
+for i=2:length(features{1})
+    
+    new = reshape(features{1}{i},[],1);
+    dbFull = [dbFull new];
+
+end
+
+%KNN Classificador
+
+%KNN = fitcknn(learnDB,learnGT,'NumNeighbors',knn_K,'NSMethod',knn_NSMethod,'Distance',knn_Distance,'Standardize',knn_Standardize);
+
+%CART Classificador
+
+%CART = fitctree(learnDB,learnGT);
+
+%SVM Classificador
+
+%svmModel = fitcecoc(learnDB,learnGT,'Learner',t);
+
+%GMM Classificador
+
+%TO-DO Configurar el classificador GMM amb la funcio fitgmdist
+
+%fitgmdist
 
 %% 2. Per a cadascuna de les 4 iteracions del 4FCV cal fer:
 for i=1:4
     %2.1 Calcular dos matrius
-
+    
     %2.2 Entrenament d’un mètode de classificació
 
     %2.3 Classificació del conjunt de test 
