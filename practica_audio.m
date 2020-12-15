c54d46dfd4e0f427beafc6d82c03d4a7a6d4223b
Alex <alex.vicente@students.salle.url.edu>
2020-12-15 18:49:26 +0100

    Correccions Splits i Matrix Concat
    LearnDB: matriu de N files (trames dels audios) i M columnes (atributs)

--- practica_audio.m
+++ practica_audio.m
@@ -73,23 +73,103 @@ features{4} = gather(features4_Tall);
 labels{4} = ads4.Labels;
 
 %1st Cell
-%TO-DO Tenim una matriu features que conté un array d'arrays de N elements
-%i M mostres (finestra) per cada N. Fer que tot estigui en una sola
-%columna, i repetir cada label per cada mostra, per a que cada una estigui
-%etiquetada correctament.
 
-dbFull = reshape(features{1}{1},[],1);
+dbFull = features{1}{1};
+
+clearvars labelsFull
+
+labelsFull(1:length(dbFull),1) = labels{1}(1);
 
 for i=2:length(features{1})
     
-    new = reshape(features{1}{i},[],1);
-    dbFull = [dbFull new];
-
+    labelWindows(1:height(features{1}{i}),1) = labels{1}(i);
+    labelsFull = [labelsFull;labelWindows];
+    dbFull = [dbFull;features{1}{i}];
+    
 end
+
+testDB = dbFull;
+testGT = labelsFull;
+
+%2nd Cell
+
+dbFull = features{2}{1};
+
+clearvars labelsFull
+
+labelsFull(1:length(dbFull),1) = labels{2}(1);
+
+for i=2:length(features{2})
+    
+    labelWindows(1:height(features{2}{i}),1) = labels{2}(i);
+    labelsFull = [labelsFull;labelWindows];
+    dbFull = [dbFull;features{2}{i}];
+    
+end
+
+learnDB1 = dbFull;
+learnGT1 = labelsFull;
+
+%3rd Cell
+
+dbFull = features{3}{1};
+
+clearvars labelsFull
+
+labelsFull(1:length(dbFull),1) = labels{3}(1);
+
+for i=2:length(features{3})
+    
+    labelWindows(1:height(features{3}{i}),1) = labels{3}(i);
+    labelsFull = [labelsFull;labelWindows];
+    dbFull = [dbFull;features{3}{i}];
+    
+end
+
+learnDB2 = dbFull;
+learnGT2 = labelsFull;
+
+%4th Cell
+
+dbFull = features{4}{1};
+
+clearvars labelsFull
+
+labelsFull(1:length(dbFull),1) = labels{4}(1);
+
+for i=2:length(features{4})
+    
+    labelWindows(1:height(features{4}{i}),1) = labels{4}(i);
+    labelsFull = [labelsFull;labelWindows];
+    dbFull = [dbFull;features{4}{i}];
+    
+end
+
+learnDB3 = dbFull;
+learnGT3 = labelsFull;
+
+%Full Data
+
+learnDB = [learnDB1;learnDB2;learnDB3];
+learnGT = [learnGT1;learnGT2;learnGT3];
 
 %KNN Classificador
 
-%KNN = fitcknn(learnDB,learnGT,'NumNeighbors',knn_K,'NSMethod',knn_NSMethod,'Distance',knn_Distance,'Standardize',knn_Standardize);
+KNN = fitcknn(learnDB,learnGT,'NumNeighbors',knn_K,'NSMethod',knn_NSMethod,'Distance',knn_Distance,'Standardize',knn_Standardize);
+
+%resultat = predict(KNN,testDB);
+
+%encerts = 0;
+
+%for i=1:length(resultat)
+    
+%    if resultat(i) == testGT(i)
+%        encerts = encerts + 1;
+%    end
+    
+%end
+
+%percentatgeErrorKNN = encerts/length(resultats) * 100;
 
 %CART Classificador
 
